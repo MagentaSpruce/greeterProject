4b244154b19b0dd830cd6de450c588f8b9ec7773
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-06-29 08:51:26 -0400

    Updates

---
+++ static/js/main.0cae67ae.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-styling-practice"]=this["webpackJsonpreact-styling-practice"]||[]).push([[0],{4:function(e,o,n){"use strict";n.r(o);n(0);var r,c=n(3),t=n.n(c),s=n(2),i=(new Date).getHours();console.log(i);var l={color:""};i<12?(r="Good Morning",l.color="red"):i<18?(r="Good Afternoon",l.color="green"):(r="Good evening",l.color="blue"),t.a.render(Object(s.jsx)("div",{children:Object(s.jsx)("h1",{className:"heading",style:l,children:r})}),document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.0cae67ae.chunk.js.map
